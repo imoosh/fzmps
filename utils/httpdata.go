f005536dfe45db5c5a68bd89cde90a68864696b9
yaowen <yaowen@centnet.com.cn>
2021-05-27 19:55:19 +0800

    添加案例宣传部分接口

--- utils/httpdata.go
+++ utils/httpdata.go
@@ -1,27 +1,30 @@
 package utils
 
 import (
-    "centnet-fzmps/common/log"
-    "encoding/json"
+	"centnet-fzmps/common/log"
+	"encoding/json"
 )
 
 type HTTPData struct {
-    Code int         `json:"code"` // 1: 成功
-    Msg  string      `json:"msg"`
-    Data interface{} `json:"data"`
+	Code int         `json:"code"` // 1: 成功
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
 }
 
 // code: 0      toast
 // code: 1      silence
 // code: 2/401  redirect to page/register/register
 func ReturnHTTPSuccess(val interface{}) interface{} {
-    var d = HTTPData{
-        Code: 1,
-        Msg:  "OK",
-        Data: val,
-    }
+	var d = HTTPData{
+		Code: 1,
+		Msg:  "OK",
+		Data: val,
+	}
 
-    js, _ := json.Marshal(&d)
-    log.Debug(string(js))
-    return d
+	js, _ := json.Marshal(&d)
+
+	if len(js) < 1024 {
+		log.Debug(string(js))
+	}
+	return d
 }
