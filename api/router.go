d28327b943ade3ae3110b71be2a6ec5f97eea7f7
yaowen <yaowen@centnet.com.cn>
2021-06-24 12:56:39 +0800

    新增获取案件信息正文部分接口

--- api/router.go
+++ api/router.go
@@ -1,98 +1,100 @@
 package api
 
 import (
-    "centnet-fzmps/api/wecom"
-    "centnet-fzmps/api/wxmp"
-    "centnet-fzmps/dao"
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"centnet-fzmps/api/wecom"
+	"centnet-fzmps/api/wxmp"
+	"centnet-fzmps/dao"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func routeInit(dao *dao.Dao) *gin.Engine {
-    router := gin.New()
-    router.Use(gin.LoggerWithWriter(&ginLogger{}), gin.Recovery(), func(c *gin.Context) { c.Set("db", dao) })
+	router := gin.New()
+	router.Use(gin.LoggerWithWriter(&ginLogger{}), gin.Recovery(), func(c *gin.Context) { c.Set("db", dao) })
 
-    // 通过微信登陆
-    router.POST("/api/wechat/login", wxmp.WeChatLogin)
+	// 通过微信登陆
+	router.POST("/api/wechat/login", wxmp.WeChatLogin)
 
-    // 更新数据
-    router.POST("/api/wechat/update", wxmp.UpdateRegisterInfo)
+	// 更新数据
+	router.POST("/api/wechat/update", wxmp.UpdateRegisterInfo)
 
-    // 完善注册信息
-    router.POST("/api/wechat/perfect", wxmp.PerfectRegisterInfo)
+	// 完善注册信息
+	router.POST("/api/wechat/perfect", wxmp.PerfectRegisterInfo)
 
-    // 获取用户信息
-    router.GET("/api/user/get", wxmp.RequestUserInfo)
+	// 获取用户信息
+	router.GET("/api/user/get", wxmp.RequestUserInfo)
 
-    // 更新用户信息
-    router.POST("/api/user/update", wxmp.UpdateUserInfo)
+	// 更新用户信息
+	router.POST("/api/user/update", wxmp.UpdateUserInfo)
 
-    // 获取字典信息
-    router.POST("/api/dict/list", wxmp.RequestDictList)
+	// 获取字典信息
+	router.POST("/api/dict/list", wxmp.RequestDictList)
 
-    // 获取家人列表
-    router.GET("/api/relative/list", wxmp.RequestFamilyMembersList)
+	// 获取家人列表
+	router.GET("/api/relative/list", wxmp.RequestFamilyMembersList)
 
-    // 删除家人信息
-    router.POST("/api/relative/delete/:id", wxmp.DeleteFamilyMember)
+	// 删除家人信息
+	router.POST("/api/relative/delete/:id", wxmp.DeleteFamilyMember)
 
-    // 添加家人信息
-    router.POST("/api/relative/add", wxmp.AddFamilyMember)
+	// 添加家人信息
+	router.POST("/api/relative/add", wxmp.AddFamilyMember)
 
-    // 更新家人信息
-    router.PUT("/api/relative/update", wxmp.UpdateFamilyMember)
+	// 更新家人信息
+	router.PUT("/api/relative/update", wxmp.UpdateFamilyMember)
 
-    // 获取家人信息
-    router.GET("/api/relative/:id", wxmp.RequestFamilyMember)
+	// 获取家人信息
+	router.GET("/api/relative/:id", wxmp.RequestFamilyMember)
 
-    // 守护详情列表
-    router.POST("/api/record/list", wxmp.RequestRecordList)
+	// 守护详情列表
+	router.POST("/api/record/list", wxmp.RequestRecordList)
 
-    // 守护详情饼状图数据
-    router.POST("/api/guard/count/pie", wxmp.RequestRecordPie)
+	// 守护详情饼状图数据
+	router.POST("/api/guard/count/pie", wxmp.RequestRecordPie)
 
-    //router.GET("/api/wechat/sysArea/getAreaInfo/:t", controllers.GetAreaInfo)
-    //router.GET("/api/wechat/sysArea/org/list/:t", controllers.GetCooperationOrgInfo)
+	//router.GET("/api/wechat/sysArea/getAreaInfo/:t", controllers.GetAreaInfo)
+	//router.GET("/api/wechat/sysArea/org/list/:t", controllers.GetCooperationOrgInfo)
 
-    // 请求预警消息
-    router.GET("/api/alarm/get", wxmp.RequestAlarm)
-    // 确认预警消息
-    router.GET("/api/alarm/confirm", wxmp.ConfirmAlarm)
+	// 请求预警消息
+	router.GET("/api/alarm/get", wxmp.RequestAlarm)
+	// 确认预警消息
+	router.GET("/api/alarm/confirm", wxmp.ConfirmAlarm)
 
-    // 请求宣传案例
-    router.GET("/api/publicityCase/get", wxmp.RequestPublicityCase)
-    // 确认宣传案例
-    router.GET("/api/publicityCase/confirm", wxmp.ConfirmPublicityCase)
+	// 请求宣传案例
+	router.GET("/api/publicityCase/get", wxmp.RequestPublicityCase)
+	// 获取案例正文
+	router.GET("/api/publicityContent/get", wxmp.RequestPublicityCaseContent)
+	// 确认宣传案例
+	router.GET("/api/publicityCase/confirm", wxmp.ConfirmPublicityCase)
 
-    // 针对企业微信后台开放接口
-    router.POST("/api/wecom/alarm", wecom.WeComVictimAlarm)
+	// 针对企业微信后台开放接口
+	router.POST("/api/wecom/alarm", wecom.WeComVictimAlarm)
 
-    authorized := router.Group("/")
-    authorized.Use(AuthRequired())
-    {
+	authorized := router.Group("/")
+	authorized.Use(AuthRequired())
+	{
 
-    }
+	}
 
-    return router
+	return router
 }
 
 //https://blog.csdn.net/qq_37767455/article/details/104712028
 // https://juejin.cn/post/6844904090690912264
 func AuthRequired() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        token := c.Request.Header.Get("token")
-        return
-        if len(token) == 0 {
-            c.JSON(http.StatusOK, gin.H{
-                "ErrCode": -1,
-                "ErrMsg":  "请求未携带token，无权限访问",
-                "Data":    nil,
-            })
-            c.Abort()
-            return
-        } else {
-            c.Next()
-        }
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
+		return
+		if len(token) == 0 {
+			c.JSON(http.StatusOK, gin.H{
+				"ErrCode": -1,
+				"ErrMsg":  "请求未携带token，无权限访问",
+				"Data":    nil,
+			})
+			c.Abort()
+			return
+		} else {
+			c.Next()
+		}
 
-    }
+	}
 }
--- api/wxmp/publicity_case.go
+++ api/wxmp/publicity_case.go
@@ -1,76 +1,101 @@
 package wxmp
 
 import (
-    "centnet-fzmps/utils"
-    "fmt"
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"centnet-fzmps/common/log"
+	"centnet-fzmps/utils"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
 type PublicityCaseResp struct {
-    Id            string `json:"id"`
-    Title         string `json:"title"`
-    Picture       string `json:"picture"`
-    Content       string `json:"content"`
-    CaseType      string `json:"case_type"`
-    Source        string `json:"source"`
-    PublisherId   string `json:"publisher_id"`
-    PublisherName string `json:"publisher_name"`
-    PublisherTime string `json:"publisher_time"`
+	Id            string `json:"id"`
+	Title         string `json:"title"`
+	Picture       string `json:"picture"`
+	Content       string `json:"content"`
+	CaseType      string `json:"case_type"`
+	Source        string `json:"source"`
+	PublisherId   string `json:"publisher_id"`
+	PublisherName string `json:"publisher_name"`
+	PublisherTime string `json:"publisher_time"`
+}
+
+type PublicityCaseContentResp struct {
+	Content string `json:"content"`
 }
 
 var CaseTypes = map[int]string{
-    1:  "带宽、代办信用卡类",
-    2:  "刷单返利类",
-    4:  "虚假购物、服务类",
-    5:  "杀猪盘类",
-    6:  "冒充公检法及政府机关类",
-    7:  "冒充领导、熟人等身份类",
-    8:  "网络游戏产品虚假交易类",
-    9:  "网络婚恋、交友类",
-    10: "虚假征信类",
-    11: "冒充军警购物类",
-    12: "其他类型",
-    13: "刷单返利类",
+	1:  "带宽、代办信用卡类",
+	2:  "刷单返利类",
+	4:  "虚假购物、服务类",
+	5:  "杀猪盘类",
+	6:  "冒充公检法及政府机关类",
+	7:  "冒充领导、熟人等身份类",
+	8:  "网络游戏产品虚假交易类",
+	9:  "网络婚恋、交友类",
+	10: "虚假征信类",
+	11: "冒充军警购物类",
+	12: "其他类型",
+	13: "刷单返利类",
 }
 
 func RequestPublicityCase(c *gin.Context) {
-    openid := c.Query("openid")
-    if len(openid) == 0 {
-        c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
-        return
-    }
+	openid := c.Query("openid")
+	if len(openid) == 0 {
+		c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
+		return
+	}
 
-    var resp []PublicityCaseResp
-    pc := DB(c).QueryPublicityCase()
-    for _, c := range pc {
-        resp = append(resp, PublicityCaseResp{
-            Id:            fmt.Sprintf("%d", c.ID),
-            Title:         c.Title,
-            Picture:       c.Picture,
-            Content:       c.Content,
-            CaseType:      CaseTypes[c.CaseType],
-            Source:        c.Source,
-            PublisherId:   fmt.Sprintf("%d", c.PublisherId),
-            PublisherName: c.PublisherName,
-            PublisherTime: c.PublisherTime.Time().Format("2006-01-02 15:04:05"),
-        })
-    }
+	var resp []PublicityCaseResp
+	pc := DB(c).QueryPublicityCase()
+	for _, c := range pc {
+		resp = append(resp, PublicityCaseResp{
+			Id:            fmt.Sprintf("%d", c.ID),
+			Title:         c.Title,
+			Picture:       c.Picture,
+			CaseType:      CaseTypes[c.CaseType],
+			Source:        c.Source,
+			PublisherId:   fmt.Sprintf("%d", c.PublisherId),
+			PublisherName: c.PublisherName,
+			PublisherTime: c.PublisherTime.Time().Format("2006-01-02 15:04:05"),
+		})
+	}
 
-    c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(&resp))
+	c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(&resp))
+}
+
+func RequestPublicityCaseContent(c *gin.Context) {
+	id := c.Query("id")
+	if len(id) == 0 {
+		c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
+		return
+	}
+
+	caseId, err := strconv.Atoi(id)
+	if err != nil {
+		log.Error(err)
+		c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
+	}
+
+	var resp *PublicityCaseContentResp
+	pc := DB(c).QueryPublicityCaseContent(caseId)
+	resp = &PublicityCaseContentResp{Content: pc.Content}
+
+	c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(resp))
 }
 
 func ConfirmPublicityCase(c *gin.Context) {
-    openid := c.Query("openid")
-    if len(openid) == 0 {
-        c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
-        return
-    }
-    caseId := c.Query("caseId")
-    if len(caseId) == 0 {
-        c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
-        return
-    }
+	openid := c.Query("openid")
+	if len(openid) == 0 {
+		c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
+		return
+	}
+	caseId := c.Query("caseId")
+	if len(caseId) == 0 {
+		c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
+		return
+	}
 
-    c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
+	c.JSON(http.StatusOK, utils.ReturnHTTPSuccess(nil))
 }
